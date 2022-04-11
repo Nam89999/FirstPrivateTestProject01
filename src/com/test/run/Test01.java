0d224816070f1f29dd3497b18e486681d6c92b6e
Nam JK <nam89@biz.kaoni.com>
2022-04-11 15:06:35 +0900

    혼자 처음올려보는 깃

---
+++ src/com/test/run/Test01.java
@@ -0,0 +1,41 @@
+package com.test.run;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		//별짓기 연습 220411
+		
+		for(int i=0; i<5; i++) 
+		{ 
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}	
+			System.out.println();	
+
+		}		
+	}
+
+//	
+//별찍기 이중for문
+//
+//for(int i=0; i<5; i++)
+//{
+//   for(int j=0; j<=1; j++)
+//	{
+//			실행문
+//	}
+//}
+//
+//i=0, 실행문 1번 (j=0)
+//i=1, 실행문 2번 (j=0,1)
+//i=2, 실행문 3번 (j=0,1,2)
+//j안의 실행문=i+1번
+//i=4이면 실행문 5번
+
+	 
+	    
+				
+				
+
+}
